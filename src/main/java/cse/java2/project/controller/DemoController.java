b16a01f497ec8a89d407eef64fe38faddde8ffd1
hsl2363 <hshaolinv@sina.com>
2023-05-30 23:56:18 +0800

    CodeStyle Optimize

--- src/main/java/cse/java2/project/controller/DemoController.java
+++ src/main/java/cse/java2/project/controller/DemoController.java
@@ -8,9 +8,12 @@ public class DemoController {
 
     /**
      * This method is called when the user requests the root URL ("/") or "/demo".
-     * In this demo, you can visit localhost:9090 or localhost:9090/demo to see the result.
+     * In this demo, you can visit localhost:9090 or localhost:9090/demo to see the
+     * result.
+     * 
      * @return the name of the view to be rendered
-     * You can find the static HTML file in src/main/resources/templates/demo.html
+     *         You can find the static HTML file in
+     *         src/main/resources/templates/demo.html
      */
     @GetMapping("/test")
     public String demo() {
